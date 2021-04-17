e265c27e2652d3406b54c54376c529d519b7889c
japanid133 <baguspw2405@gmail.com>
2021-04-18 04:28:24 +0700

    implement insert function

--- app/Http/Controllers/ArchiveController.php
+++ app/Http/Controllers/ArchiveController.php
@@ -1,4 +1,7 @@
-<?php /** @noinspection SpellCheckingInspection */
+<?php /** @noinspection PhpUndefinedMethodInspection */
+/** @noinspection PhpUndefinedFieldInspection */
+
+/** @noinspection SpellCheckingInspection */
 
 namespace App\Http\Controllers;
 
@@ -7,6 +10,7 @@ use App\Http\back\_App;
 use App\Http\back\_Authorize;
 use App\Http\back\_Log;
 use App\Http\back\_UI;
+use App\Http\back\authorize\Archive;
 use App\Models\Archivetype;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -49,7 +53,32 @@ class ArchiveController extends Controller
     }
 
     public function insertType(Request $request):JsonResponse {
+        _Log::log(_Log::$INFO,'sending request insert new archivetype');
+        $name = $request->name;
+        if (_Authorize::manage(Archive::class)) {
+            if (Archivetype::all()->where('name', $name)->count() == 0) {
+                $new        = new Archivetype();
+                $new->name  = $name;
+                $code       = Archivetype::makeCode();
+                $new->code  = $code;
+                $new->token = Archivetype::makeToken();
+                $new->folder_path = Archivetype::makeDir($code);
+                $new->officer()->associate(_Authorize::data()->officer);
+                $new->save();
+                $new = Archivetype::with(['officer','officer.user','archives','archives.archivefiles','archives.officer','archives.officer.user'])->firstWhere('id', $new->id);
+                $status  = ['status'=>'success','message'=>'Berhasil menambahkan tipe arsip', 'archivetype'=>$new];
+                _Log::log(_Log::$SUCCESS,'sending request insert new archivetype success');
+                _Activity::do('menambahkan tipe arsip ' . $name);
+            } else {
+                $status = ['status'=>'error','message'=>'Arsip '.$name.' sudah tersedia'];
+                _Log::log(_Log::$DANGER,'sending request insert new archivetype failed');
+            }
+        } else {
+            $status = ['status'=>'error','message'=>'Kesalahan otorisasi akun pegawai'];
+            _Log::log(_Log::$DANGER,'sending request insert new archivetype failed');
+        }
 
+        return response()->json(array_merge($request->all(), $status));
     }
 
     public function editType(Request $request):JsonResponse {
